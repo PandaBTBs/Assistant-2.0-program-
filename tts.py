23a5e2034aee35b17c107b6ae8f93dd10ea05e3b
sweet_gream <118614536+PandaBTBs@users.noreply.github.com>
2024-06-07 21:33:27 +0300

    Update tts.py

--- tts.py
+++ tts.py
@@ -26,6 +26,12 @@ def model_change(speech):
         return 3
     elif speech == 'xenia':
         return 3
+    
+def model_vosk(voskSTT):
+    if voskSTT == 'big':
+        return 1
+    elif voskSTT == 'small':
+        return 2
 
 
 out_red('pls input start....')
@@ -58,9 +64,13 @@ if n ==  'start':
         
     elif p == 'model':
         print('input vosk model: small or big')
-        vosk_model = str(input())
-        if vosk_model == 'small' or vosk_model == 'big':
-            pass 
+        voskSTT = str(input())
+        if model_vosk(voskSTT) == 1:
+            print('model_vosk finish installed: big')
+            pass
+        elif model_vosk(voskSTT) == 2:
+            print('model_vosk finish installed: small')
+            pass
         
         else:
             out_red('error, EXIT')
@@ -75,13 +85,13 @@ if n ==  'start':
         
             
         if model_change(speech) == 1:
-            out_red('model_vosk and speech finish installed: aidar')
+            out_red('speech finish installed: aidar')
             out_red('-------------------------start-------------------------')
             out_white('text-auto')
             speaker = 'aidar' 
     
         elif model_change(speech) == 2:
-            out_red('model_vosk and speech finish installed: baya')
+            out_red('speech finish installed: baya')
             out_red('-------------------------start-------------------------')
             out_white('text-auto')
             speaker = 'baya'
@@ -120,29 +130,9 @@ else:
                 proc.terminate()
 
 
-
-# print('pls input speek model: aidar, baya, kseniya, xenia')
-
-# a = str(input())
-
-
 language = 'ru'
 model_id = 'ru_v3'
 sample_rate = 48000 # 48000
-# aidar, baya, kseniya, xenia
-
-
-# if model_change(a) == 1:
-#     speaker = 'aidar' 
-    
-# elif model_change(a) == 2:
-#     speaker = 'baya'
-    
-# elif model_change(a) == 3:
-#     speaker = 'kseniya'
-    
-# elif model_change(a) == 4:
-#     speaker = 'xenia'
 
 put_accent = True
 put_yo = True
@@ -157,7 +147,7 @@ model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
 model.to(device)
 
 
-# воспроизведение
+
 def va_speak(what: str):
     audio = model.apply_tts(text=what+"..",
                             speaker=speaker,
@@ -168,59 +158,3 @@ def va_speak(what: str):
     sd.play(audio, sample_rate * 1.05)
     time.sleep((len(audio) / sample_rate) + 0.5 )
     sd.stop()
-
-# va_speak(text)
-# sd.play(audio, sample_rate)
-# time.sleep(len(audio) / sample_rate)
-# sd.stop()
-
-# test
-
-# import torch
-# import sounddevice as sd
-# import time
-# import app
-
-
-
-# language = 'ru'
-# model_id = 'ru_v3'
-# sample_rate = 48000 # 48000
-
-
-# speaker = 'baya' # aidar, baya, kseniya, xenia
-
-# def name():
-#     if app.aidar == 1:
-#         print('using Aidar')
-#         speaker = 'aidar'
-#     elif app.baya == 2:
-#         print('using baya')
-#         speaker = 'baya'
-# put_accent = True
-# put_yo = True
-# device = torch.device('cpu') # cpu или gpu
-# text = 'тест голоса ассистента с разными моделями речи'
-
-
-# model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
-#                           model='silero_tts',
-#                           language=language,
-#                           speaker=model_id)
-# model.to(device)
-
-
-# # воспроизведение
-
-# audio = model.apply_tts(text=text,
-#                         speaker=speaker,
-#                         sample_rate=sample_rate,
-#                         put_accent=put_accent,
-#                         put_yo=put_yo)
-    
-# print(text)
-
-# # va_speak(text)
-# sd.play(audio, sample_rate )
-# time.sleep((len(audio) / sample_rate))
-# sd.stop()
