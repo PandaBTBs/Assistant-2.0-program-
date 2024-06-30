d26e21e08384bba8df283815d190051602609ec4
sweet_gream <118614536+PandaBTBs@users.noreply.github.com>
2024-06-30 16:29:35 +0300

    Update log.py

--- log.py
+++ log.py
@@ -1,52 +1,44 @@
-import json 
+# name = tuple(j134["name"])
+# alt_name = tuple(j134["alt_name"])
+# short_programs = tuple(j134["short_programs"])
 
-with open('J:\\Pack_Ai\\Stella\\stellaPR\\j134.json', 'r', encoding='utf-8') as json_file:
-	j134 = json.load(json_file)
+name = ('ассистент')
 
-with open('J:\\Pack_Ai\\Stella\\stellaPR\\j134.json', 'w', encoding='utf-8') as json_file:
-	json.dump(j134, json_file, indent=2)
+alt_name = ('ассистент', 'робот', 'алиса')
 
-
-print(j134["cmd_list"])
-
-
-name = tuple(j134["name"])
-
-alt_name = tuple(j134["alt_name"])
-
-short_programs = tuple(j134["short_programs"])
+short_programs = ('ответь', 'переведи', 'скажи', 'говори', 'сколько вермя')
 
 cmd_list = {
         "list": ('список команд', 'команды', 'что ты умеешь'),
-        
+
         "time": ('время', 'текущее время', 'сейчас времени', 'который час'),
-        
+
         "open_browser": ( 'гугл хром', 'браузер'),
-        
+
         "history": ('тетрадь'),
-        
+
         "open_browser_steam": ('стим', 'запусти стим'),
-        
+
         "open_browser_server": ('сервер', 'запусти сервер'),
-    
+
         "open_sharex": ('снимок экрана', 'запусти снимок'),
-        
+
         "open_OBS": ('запись', 'запись экрана'),
-        
+
         "open_vs": ('код', 'программирование'),
-        
+
         "open_vtube": ('аватар', 'виртуальная студия'),
-        
+
         "open_discord": ('общение', 'друзья'),
-        
+
         "joke": ('расскажи анекдот', 'шутка', 'расскажи шутку', 'пошути'),
-        
+
         "history": ('истории','навык'),
-        
+
         "chat_llama": ('Ответ', 'Вопрос'),
-        
+
         "work_zone": ('рабочая зона', 'зона'),
-        
+
         "exit": ( 'заврешение работы', 'конец работы', 'закрыть')
-        
+
     }
